f7c76b8254db03f0baa795dfd86c8439301b6af1
Chris Meisner <chris@chrismeisner.com>
2026-01-03 12:56:59 -0500

    feat: add media constants

---
+++ src/constants/media.ts
@@ -0,0 +1,6 @@
+export type MediaSource = "local" | "remote";
+
+export const MEDIA_SOURCE_KEY = "mediaSource";
+export const MEDIA_SOURCE_EVENT = "media-source-updated";
+export const REMOTE_MEDIA_BASE = "https://chrismeisner.com/media/";
+
